1849c9687fadf684f2500ca9070ed6868ec50b7c
Orhan Gölcür <orhangolcur0@gmail.com>
2025-08-12 15:05:45 +0300

    Complete order

--- src/app/dialogs/order-detail-dialog/order-detail-dialog.component.ts
+++ src/app/dialogs/order-detail-dialog/order-detail-dialog.component.ts
@@ -3,6 +3,11 @@ import { BaseDialog } from '../base/base-dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { OrderService } from 'src/app/services/common/models/order.service';
 import { Single_Order } from 'src/app/contracts/order/single_order';
+import { DialogService } from 'src/app/services/common/dialog.service';
+import { CompleteOrderDialogComponent, CompleteOrderState } from '../complete-order-dialog/complete-order-dialog.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-order-detail-dialog',
@@ -12,7 +17,10 @@ import { Single_Order } from 'src/app/contracts/order/single_order';
 export class OrderDetailDialogComponent extends BaseDialog<OrderDetailDialogComponent> implements OnInit {
   constructor(dialogRef: MatDialogRef<OrderDetailDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: OrderDetailDialogState | string,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private dialogService: DialogService,
+    private spinner: NgxSpinnerService,
+    private toastrService: CustomToastrService
   ) {
     super(dialogRef);
   }
@@ -29,6 +37,22 @@ export class OrderDetailDialogComponent extends BaseDialog<OrderDetailDialogComp
     this.dataSource = this.singleOrder.basketItems;
 
     this.totalPrice = this.singleOrder.basketItems.map((basketItem, index) => basketItem.price * basketItem.quantity).reduce((price, current) => price + current);
+  }
+
+  completeOrder() {
+    this.dialogService.openDialog({
+      componentType: CompleteOrderDialogComponent,
+      data: CompleteOrderState.Yes,
+      afterClosed: async () => {
+        this.spinner.show(SpinnerType.BallAtom);
+        await this.orderService.completeOrder(this.data as string);
+        this.spinner.hide(SpinnerType.BallAtom);
+        this.toastrService.message("Sipariş başarıyla tamamlanmıştır! Müşteriye bilgi verilmiştir.", "Sipariş Tamamlandı!", {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight
+        });
+      }
+    });
   }
 }
 
